6fdf670d58dde88d407e34ae1603a4b97d7bad14
thanhdanh27 <maithanhdanh112@gmail.com>
2025-02-04 17:52:12 +0700

    first deploy

---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+  { name: "Home", link: "#" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#recentProjects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Blog Site",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/codeblog.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "NEST SHOPPING",
+    des: "Explore a user-friendly and modern shopping experience with this sleek, responsive e-commerce front-end, built with ReactJS.",
+    img: "/shop.png",
+    iconLists: ["/re.svg", "/boot.svg", "/ant.svg"],
+    link: "https://thanhdanh27.github.io/Nest-Shopping/",
+  },
+  {
+    id: 2,
+    title: "HISTOLINGO",
+    des: "Discover the rich history of Vietnam through an engaging and interactive learning platform, designed to help users explore historical events, figures, and milestones.",
+    img: "/his.png",
+    iconLists: ["/re.svg", "/ant.svg", "/express.svg", "/cloud.svg"],
+    link: "https://histolingo-player-fndpfeekbzf6fdbc.eastasia-01.azurewebsites.net/",
+  },
+  {
+    id: 3,
+    title: "NEXUS HOTEL",
+    des: "A comprehensive hotel management system, designed to streamline bookings, guest services, and operations, offering an intuitive interface and powerful features to enhance the overall hotel experience.",
+    img: "/ks.png",
+    iconLists: ["/re.svg", "/ant.svg", "/ts.svg", "/angu.svg"],
+    link: "https://homepage-hotel.web.app/",
+  },
+  {
+    id: 4,
+    title: "D-Movie",
+    des: "A dynamic movie platform that allows users to explore, review, and watch movies across various genres, with an intuitive interface and personalized recommendations.",
+    img: "/movie.png",
+    iconLists: ["/re.svg", "/ant.svg", "/boot.svg"],
+    link: "https://d-movie-1db26.web.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Thanh Danh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Thanh Danh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Thanh Danh is the ideal partner.",
+    name: "Mark Zuckerberg",
+    title: "Co-founder of Meta",
+  },
+  {
+    quote:
+      "Collaborating with Thanh Danh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Thanh Danh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Thanh Danh is the ideal partner.",
+    name: "Mark Zuckerberg",
+    title: "Co-founder of Meta",
+  },
+  {
+    quote:
+      "Collaborating with Thanh Danh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Thanh Danh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Thanh Danh is the ideal partner.",
+    name: "Mark Zuckerberg",
+    title: "Co-founder of Meta",
+  },
+  {
+    quote:
+      "Collaborating with Thanh Danh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Thanh Danh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Thanh Danh is the ideal partner.",
+    name: "Mark Zuckerberg",
+    title: "Co-founder of Meta",
+  },
+  {
+    quote:
+      "Collaborating with Thanh Danh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Thanh Danh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Thanh Danh is the ideal partner.",
+    name: "Mark Zuckerberg",
+    title: "Co-founder of Meta",
+  },
+  {
+    quote:
+      "Collaborating with Thanh Danh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Thanh Danh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Thanh Danh is the ideal partner.",
+    name: "Mark Zuckerberg",
+    title: "Co-founder of Meta",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Backend Engineer Intern",
+    desc: "Developed and optimized RESTful APIs using NestJS and TypeScript, improving backend performance and scalability.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Developer",
+    desc: "Led the development of a web application for a client, from initial concept to deployment, ensuring responsive design and seamless user experience.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Team-Oriented Developer",
+    desc: "Collaborated effectively with team members, contributed to group discussions, and supported peers to achieve project goals efficiently.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/thanhdanh27",
+  },
+  {
+    id: 2,
+    img: "/fac.svg",
+    url: "https://www.facebook.com/danh1.12",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/thanh-danh-mai-94010332b/",
+  },
+];
