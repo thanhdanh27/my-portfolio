78def325db8fe7c1a7a838f10e65559ea26d3c5a
thanhdanh27 <maithanhdanh112@gmail.com>
2025-02-04 21:39:59 +0700

    deploy again

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,13 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        {/* <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
-        <Approach />
+        <Approach /> */}
         <Footer />
       </div>
     </main>
