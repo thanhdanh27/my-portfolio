513f5f4e3ebd6d7e7b3e6a2214d0c8b073e8e16b
thanhdanh27 <maithanhdanh112@gmail.com>
2025-02-04 22:06:59 +0700

    again

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -183,13 +183,13 @@ export const BentoGridItem = ({
                 {/* <Lottie options={defaultOptions} height={200} width={400} /> */}
               </div>
 
-              {/* <MagicButton
+              <MagicButton
                 title={copied ? "Email is Copied!" : "Copy my email address"}
                 icon={<IoCopyOutline />}
                 position="left"
                 handleClick={handleCopy}
                 otherClasses="!bg-[#161A31]"
-              /> */}
+              />
             </div>
           )}
         </div>
