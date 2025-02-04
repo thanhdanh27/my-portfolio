b6411df01b130a0c9c725713d96a5d2d46c155f8
thanhdanh27 <maithanhdanh112@gmail.com>
2025-02-04 21:48:09 +0700

    again

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         {/* <Hero />
-        <Grid />
-        <RecentProjects /> */}
+        <Grid /> */}
+        <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
