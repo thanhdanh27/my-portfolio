def31b7eb4cfad1b0616539332f437a727997d6b
thanhdanh27 <maithanhdanh112@gmail.com>
2025-02-04 21:50:07 +0700

    again

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        {/* <Hero />
-        <Grid /> */}
+        <Hero />
+        {/* <Grid /> */}
         <RecentProjects />
         <Clients />
         <Experience />
