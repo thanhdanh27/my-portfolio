45bf1b6143b29a4b078f6e38fd0b4f3b77e5664c
thanhdanh27 <maithanhdanh112@gmail.com>
2025-02-04 22:39:59 +0700

    again

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -9,7 +9,6 @@ import { BackgroundGradientAnimation } from "./GradientBg";
 
 import animationData from "../../data/confetti.json";
 import MagicButton from "../MagicButton";
-import GridGlobe from "@/components/ui/GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -135,9 +134,6 @@ export const BentoGridItem = ({
           >
             {title}
           </div>
-
-          {/* for the github 3d globe */}
-          {/* {id === 2 && <GridGlobe />} */}
 
           {/* Tech stack list div */}
           {id === 3 && (
